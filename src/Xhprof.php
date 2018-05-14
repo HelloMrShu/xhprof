1b3f2fb6745d82a9093c0948a78017f48d4d10f6
gaoqi <gaoqi@yunniao.me>
2018-05-14 14:39:11 +0800

    feat: xhprof plugin development

---
+++ src/Xhprof.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace silly\xhprof;
+
+
+class Xhprof
+{
+    /* 
+     * function xhprof_start 
+     * start to analyze the php code
+     */
+    public function xhprof_start(){
+        xhprof_enable(XHPROF_FLAGS_NO_BUILTINS | XHPROF_FLAGS_CPU | XHPROF_FLAGS_MEMORY);
+    }   
+
+
+    /*  
+     * function xhporf_end
+     * end to analyze the php code
+     * and print the result to file that configured in php.ini
+     */
+    public function xhprof_end(){
+
+        $xhprof_data = xhprof_disable();                                                                          
+        return $xhprof_data;
+    }   
+
+    /*
+     * function xhprof_display
+     * display the data
+     */
+    public function xhprof_display($data, $path = '', $source = 'xhprof', $host = '') {
+        
+        include_once("xhprof_lib.php");                                                             
+        include_once("xhprof_runs.php");                                                            
+
+        $xhprofRuns = new XHProfRuns_Default();    
+        $runId = $xhprofRuns->save_run($data, $source);    
+                                                                                                                  
+        echo $host . $runId . '&source='.$source; 
+    }
+}
